55ed1ce7d5af3eaffd1094d56cafc483e3da661d
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 12:06:33 -0500

    Primer Comit para repositor remoto =

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,21 @@
+
+package operaciones;
+
+
+public class Operaciones {
+
+   
+    public static void main(String[] args) {
+      int numero1 = 4;
+      int numero2 = 4;
+      
+      int suma = numero1+numero2;
+      int resta = numero1-numero2; 
+      int mu1tip1icacion = numero1*numero2;
+      int divicion = numero1/numero2;
+      
+      System.out.println("El resultado de la suma es: "+suma+" \nLa resta es:"+ resta+ "\nLa mu1tip1icacion es:"+mu1tip1icacion+"\nLa divicion es:"+ divicion);
+      
+    }
+    
+}
